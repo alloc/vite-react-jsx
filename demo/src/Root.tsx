d56b260c0206a2974dde2bb9c4a0753039a7f9a2
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-05-23 14:10:34 -0400

    demo: use react-dropzone
    This packages has an ESM entry point

--- demo/src/Root.tsx
+++ demo/src/Root.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import One from 'react-one'
 import Two from 'react-two'
 import Switch from 'react-switch'
+import Dropzone from 'react-dropzone'
 
 export const Root = () => {
   const [checked, setChecked] = useState(false)
@@ -10,6 +11,36 @@ export const Root = () => {
       <One />
       <Two />
       <Switch checked={checked} onChange={setChecked} />
+      <FileZone />
     </>
   )
 }
+
+const FileZone = () => (
+  <div style={{ width: 200, margin: '50px auto' }}>
+    <Dropzone>
+      {state => (
+        <div {...state.getRootProps()}>
+          <input {...state.getInputProps()} />
+          <div
+            style={{
+              width: 200,
+              height: 200,
+              borderRadius: 10,
+              background: `hsla(0, 0%, 100%, ${
+                state.isDragActive ? 0.3 : 0.1
+              })`,
+              color: 'white',
+              fontSize: 14,
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              userSelect: 'none',
+            }}>
+            Drop a file on me!
+          </div>
+        </div>
+      )}
+    </Dropzone>
+  </div>
+)
