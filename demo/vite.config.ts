fcad0955716ba3fd636fbd5b5ae50eb3bb6ba624
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-05-23 14:40:50 -0400

    demo: remove `mode: development` from vite config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,9 +4,6 @@ import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
   plugins: [reactRefresh(), reactJsx()],
-  // Removing this line will break the bundle, because react-switch
-  // is pre-minified and therefore is not supported.
-  mode: 'development',
   build: {
     // The minified bundle works as expected.
     minify: false,
