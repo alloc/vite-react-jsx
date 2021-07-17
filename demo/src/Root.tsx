771eed719252a2c77004551a93f70fe067c084bb
Eddy Nguyen <eddeee.nguyen@gmail.com>
2021-07-17 11:57:26 +1000

    Set up repo with optimizeDep problem

--- demo/src/Root.tsx
+++ demo/src/Root.tsx
@@ -1,54 +1,7 @@
-import { useState } from 'react'
-
-// This local package uses the automatic JSX runtime in a .jsx module
-import One from 'react-one'
-
-// This local package uses `import React from 'react'` in a .tsx module
-import Two from 'react-two'
-
-// This package has a minified CJS entry point and a development module
-import { Switch } from './deps'
-
-// This package has a ESM entry point
-import Dropzone from 'react-dropzone'
+// PROBLEM: This is an alias path set in tsconfig.json
+// This points to src/shared/ui which has an index.ts file that exports stuff
+import { File } from '@/shared/ui'
 
 export const Root = () => {
-  const [checked, setChecked] = useState(false)
-  return (
-    <>
-      <One />
-      <Two />
-      <Switch checked={checked} onChange={setChecked} />
-      <FileZone />
-    </>
-  )
+  return <File />
 }
-
-const FileZone = () => (
-  <div style={{ width: 200, margin: '50px auto' }}>
-    <Dropzone>
-      {state => (
-        <div {...state.getRootProps()}>
-          <input {...state.getInputProps()} />
-          <div
-            style={{
-              width: 200,
-              height: 200,
-              borderRadius: 10,
-              background: `hsla(0, 0%, 100%, ${
-                state.isDragActive ? 0.3 : 0.1
-              })`,
-              color: 'white',
-              fontSize: 14,
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              userSelect: 'none',
-            }}>
-            Drop a file on me!
-          </div>
-        </div>
-      )}
-    </Dropzone>
-  </div>
-)
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,14 +1,15 @@
 import reactJsx from 'vite-react-jsx'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import type { UserConfig } from 'vite'
 
 const config: UserConfig = {
-  plugins: [reactRefresh(), reactJsx()],
-  build: {
-    // The minified bundle works as expected.
-    minify: false,
-    // Source maps are generated properly.
-    sourcemap: true,
+  plugins: [reactRefresh(), tsconfigPaths(), reactJsx()],
+  // PROBLEM: Current setup does not work with `optimizeDeps`
+  // It will say something like this in the browser console:
+  // File.tsx:3 Uncaught ReferenceError: React is not defined
+  optimizeDeps: {
+    include: ['@/shared/ui'],
   },
 }
 
